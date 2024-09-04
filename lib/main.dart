6d9abe87d3696fe7d94de19cf86e14a8c6bcab4f
fhuget <teguhfaddila@gmail.com>
2024-09-04 12:18:30 +0700

    Flutter FIX with find pdf

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:komatsu_diagnostic/splash_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyCGc3Y1v4-iib5ayywFuunIIttYIhPfb0c",
+      appId: "1:612392661218:android:9f0928b7841be33990aebb",
+      messagingSenderId: "612392661218",
+      projectId: "komatsu-diagnostic",
+      storageBucket: "gs://komatsu-diagnostic.appspot.com",
+    ),
+  );
+
+  runApp(const MaterialApp(
+    title: "Komatsu Diagnostic",
+    home: SplashScreen(),
+  ));
+}
